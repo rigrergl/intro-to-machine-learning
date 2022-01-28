20d4b9cd4f1f86ac123e8762ad5c347fc6428175
rigrergl <rigrergl@gmail>
2022-01-28 16:32:13 -0600

    dataset created

---
+++ Quizzes/Quiz-1/Quiz-1.R
@@ -0,0 +1,60 @@
+# Name: Rigre R. Garciandia
+# NetID: RRG190004
+# Section: CS 4375.002
+
+fname <- c(
+  "Lenny",
+  "John",
+  "Ugun",
+  "Ulysses",
+  "Homer",
+  "Alec",
+  "Hellen",
+  "Eren",
+  "Levi",
+  "Erwin"
+)
+
+lname <- c(
+  "Smith",
+  "Norwood",
+  "Lowood",
+  "Udin",
+  "Gallagher",
+  "Watson",
+  "DelCastillo",
+  "Yaeger",
+  "Strauss",
+  "Smith"
+)
+
+zipcode <- c(
+  75093,
+  75093,
+  75094,
+  75095,
+  75096,
+  75097,
+  75098,
+  75099,
+  75094,
+  75095
+)
+
+gpa <- c(
+ 3.2,
+ 3.3,
+ 3.1,
+ 3.6,
+ 3.7,
+ 3.8,
+ 3.9,
+ 3.4,
+ 3.5,
+ 2.3
+)
+
+students.data <- data.frame(fname, lname, zipcode, gpa)
+
+students.data$fname
+
