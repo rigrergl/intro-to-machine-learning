0763ea4fec72a5ce950ac4591e16a612da8381d3
rigrergl <rigrergl@gmail>
2022-03-26 21:27:02 -0500

    finished part 2, corrected some mistakes in part 1

--- Assignments/Assignment-2/Assignment-1/Assignment-2.R
+++ Assignments/Assignment-2/Assignment-1/Assignment-2.R
@@ -31,13 +31,12 @@ tree_default
 
 rpart.plot(tree_default, extra = 2, under = TRUE, varlen = 0, faclen = 0)
 
-pred <- predict(tree_default, clean.hepatitis, type = "class")
+pred_default <- predict(tree_default, clean.hepatitis[, -20], type = "class")
 
 #Evaluate Model
-default_accuracy <- accuracy(clean.hepatitis$class, pred)
-default_accuracy
+default_accuracy <- accuracy(clean.hepatitis$class, pred_default)
 
-confusion_table <- table(clean.hepatitis$class, pred)
+confusion_table <- table(clean.hepatitis$class, pred_default)
 
 tp <- confusion_table["live", "live"]
 tn <- confusion_table["die", "die"]
@@ -57,22 +56,65 @@ fnr <- fn / (fn + tp)
 fnr
 
 #Display the actual class label and the predicted class label for the first ten objects of the improved Hepatitis data set. Find the error rate for these ten objects. 
-predicted_classes <- pred[1:10]
-actual_classes <- clean.hepatitis$class[1:10]
-
-display_df <- data.frame(predicted_classes, actual_classes)
-display_df
-
-wrong_pred_count <- nrow(display_df[display_df$predicted_classes != display_df$actual_classes,])
-error_rate <- wrong_pred_count / nrow(display_df)
+confusion_table_head <- table(clean.hepatitis$class[1:10], pred_default[1:10])
+wrong_pred_count_head <- confusion_table_head["die", "live"] + confusion_table["live", "die"]
+error_rate <- wrong_pred_count_head / 10
 error_rate
 
 #After finding the training error, estimate generalization error for this tree using pessimistic and optimistic approach. 
-generalization_df <- data.frame(pred, clean.hepatitis$class)
+wrong_pred_count_default <- confusion_table["die", "live"] + confusion_table["live", "die"]
 
-gen_wrong_pred_count <- nrow(generalization_df[generalization_df$predicted_classes != generalization_df$clean.hepatitis.class,])
-gen_error_optimistic <- gen_wrong_pred_count / nrow(generalization_df)
-gen_error_optimistic
+gen_error_optimistic_default <- wrong_pred_count_default / nrow(clean.hepatitis)
+gen_error_optimistic_default
 
-gen_error_pessimistic <- (gen_wrong_pred_count + nrow(clean.hepatitis) * 0.5) / nrow(generalization_df)
-gen_error_pessimistic
+gen_error_pessimistic_default <- (wrong_pred_count_default + nrow(clean.hepatitis) * 0.5) / nrow(clean.hepatitis)
+gen_error_pessimistic_default
+
+##################### Part 2 ##########################
+
+#Create a fully grown decision tree
+tree_full <- rpart(class ~., data = clean.hepatitis, control = rpart.control(minsplit = 2, cp = 0))
+rpart.plot(tree_full, extra = 2, under = TRUE,  varlen=0, faclen=0)
+
+pred_full <- predict(tree_full, clean.hepatitis, type = "class")
+
+#Evaluate Model
+full_accuracy <- accuracy(clean.hepatitis$class, pred_full)
+full_accuracy
+
+confusion_table_full_tree <- table(clean.hepatitis$class, pred_full)
+
+tp <- confusion_table_full_tree["live", "live"]
+tn <- confusion_table_full_tree["die", "die"]
+fn <- confusion_table_full_tree["live", "die"]
+fp <- confusion_table_full_tree["die", "live"]
+
+tpr <- tp / (tp + fn) 
+tpr
+
+fpr <- fp / (fp + tn)
+fpr 
+
+tnr <- tn / (tn + fp)
+tnr 
+
+fnr <- fn / (fn + tp)
+fnr
+
+actual_classes <- clean.hepatitis$class
+
+comparison_df <- data.frame(pred_full, actual_classes)
+comparison_df
+
+wrong_pred_count <- nrow(comparison_df[comparison_df$predicted_classes != comparison_df$actual_classes,])
+error_rate_full <- wrong_pred_count / nrow(comparison_df)
+error_rate_full
+
+#After finding the training error, estimate generalization error for this tree using pessimistic and optimistic approach. 
+wrong_pred_count_full <- confusion_table_full_tree["die", "live"] + confusion_table_full_tree["live", "die"]
+
+gen_error_optimistic_full <- wrong_pred_count_full / nrow(clean.hepatitis)
+gen_error_optimistic_full
+
+gen_error_pessimistic_full <- (wrong_pred_count_full + nrow(clean.hepatitis) * 0.5) / nrow(clean.hepatitis)
+gen_error_pessimistic_full
